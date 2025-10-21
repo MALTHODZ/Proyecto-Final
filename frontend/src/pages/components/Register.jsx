b086779624e9fd671deeb8b6e21e258ed7e3ca77
malthodz <emprendedorxavier@gmail.com>
2025-10-21 16:27:09 +0200

    Componente register usando Formik con los campos de nombre y correo

--- frontend/src/pages/components/Register.jsx
+++ frontend/src/pages/components/Register.jsx
@@ -1,56 +1,68 @@
-import { Formik } from "formik";
+import {useState} from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function Register () {
+
+    const [sendForm, setSendForm] = useState(false)
+
     return(
     <div>
         <Formik
             initialValues={{
-                nombre:'Babunger',
-                correo:'Babunger@gmail.com',
+                nombre:'',
+                correo:'',
             }}
             validate={(values) => {
                 let error = {};
                 if (!values.nombre) {
                     error.nombre = 'Por favor introduce un nombre';
-                } else if(/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)){
+                } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)){
                     error.nombre = 'El nombre solo puede contener letras y espacios';
                     }
+                if (!values.correo) {
+                    error.correo = 'Por favor introduce un correo';
+                } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.correo)){
+                    error.correo = 'El correo solo puede contener letras, numeros, puntos y guiones';
+                }
                 return error;
             }}
-            onSubmit={()=>{
+            //Envia el formulario donde yo quiera y resetea el formulario
+            onSubmit={(values, { resetForm })=>{
+                resetForm();
                 console.log("enviado")
+                setSendForm(true)
+                setTimeout(() => setSendForm(false),3000)
             }}
         >
-            {({values, errors, handleSubmit, handleChange,handleBlur}) => (
-                <form onSubmit={handleSubmit}>
+            {({errors}) => (
+                <Form>
                     <div>
                         <label htmlFor="nombre">Nombre</label>
-                        <input
+                        <Field
                             type="text"
                             id="nombre"
                             name="nombre"
                             placeholder="Nombre de usuario"
-                            value={values.nombre}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
                         />
-                        {errors.nombre && <div>{errors.nombre}</div>}
+                        <ErrorMessage name="nombre" component={() => (
+                            <div className="error">{errors.nombre}</div>
+                        )}/>
                     </div>
                     <div>
                         <label htmlFor="correo">Correo</label>
-                        <input
+                        <Field
                             type="text"
                             id="correo"
                             name="correo"
                             placeholder="nombre@correo.com"
-                            value={values.correo}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
                         />
-                        {errors.correo && <div>{errors.correo}</div>}
+                        <ErrorMessage name="correo" component={() => (
+                            <div className="error">{errors.correo}</div>
+                        )}/>
                     </div>
                     <button type="submit">Register</button>
-                </form>
+                    {sendForm && <p>Te has registrado con exito!</p>}
+                </Form>
             )}
         </Formik>
     </div>
