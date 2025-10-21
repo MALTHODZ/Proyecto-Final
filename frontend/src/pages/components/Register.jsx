b85af8fbf1493e7eec443561fed32e0f99602aa0
malthodz <emprendedorxavier@gmail.com>
2025-10-21 17:18:33 +0200

    Implementada la validacion de contraseña en Register

--- frontend/src/pages/components/Register.jsx
+++ frontend/src/pages/components/Register.jsx
@@ -11,18 +11,34 @@ export default function Register () {
             initialValues={{
                 nombre:'',
                 correo:'',
+                password1:'',
+                password2:''
             }}
             validate={(values) => {
                 let error = {};
+
                 if (!values.nombre) {
                     error.nombre = 'Por favor introduce un nombre';
                 } else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)){
                     error.nombre = 'El nombre solo puede contener letras y espacios';
                     }
+
                 if (!values.correo) {
                     error.correo = 'Por favor introduce un correo';
                 } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.correo)){
                     error.correo = 'El correo solo puede contener letras, numeros, puntos y guiones';
+                }
+
+                if (!values.password1) {
+                    error.password1 = 'Por favor introduce una contraseña';
+                } else if (values.password1.length < 6) {
+                    error.password1 = 'La contraseña debe tener al menos 6 caracteres';
+                }
+
+                if (!values.password2) {
+                    error.password2 = 'Por favor confirma tu contraseña';
+                } else if (values.password1 !== values.password2) {
+                    error.password2 = 'Las contraseñas no coinciden';
                 }
                 return error;
             }}
@@ -58,6 +74,30 @@ export default function Register () {
                         />
                         <ErrorMessage name="correo" component={() => (
                             <div className="error">{errors.correo}</div>
+                        )}/>
+                    </div>
+                    <div>
+                        <label htmlFor="contraseña">Contraseña</label>
+                        <Field
+                            type="password"
+                            id="password1"
+                            name="password1"
+                            placeholder="crea una contraseña"
+                        />
+                        <ErrorMessage name="password1" component={() => (
+                            <div className="error">{errors.password1}</div>
+                        )}/>
+                    </div>
+                    <div>
+                        <label htmlFor="contraseña">Contraseña</label>
+                        <Field
+                            type="password"
+                            id="password2"
+                            name="password2"
+                            placeholder="pon la misma contraseña"
+                        />
+                        <ErrorMessage name="password2" component={() => (
+                            <div className="error">{errors.password2}</div>
                         )}/>
                     </div>
                     <button type="submit">Register</button>
