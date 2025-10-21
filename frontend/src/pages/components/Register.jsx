48b59556358342b2ab63838b7224b9b0d4428375
malthodz <emprendedorxavier@gmail.com>
2025-10-21 15:17:37 +0200

    Componente register con campos de nombre y correo mas la validacion de nombre

--- frontend/src/pages/components/Register.jsx
+++ frontend/src/pages/components/Register.jsx
@@ -1,11 +1,58 @@
-
+import { Formik } from "formik";
 
 export default function Register () {
     return(
     <div>
-        <form>
-            <label>Nombre</label>
-        </form>
+        <Formik
+            initialValues={{
+                nombre:'Babunger',
+                correo:'Babunger@gmail.com',
+            }}
+            validate={(values) => {
+                let error = {};
+                if (!values.nombre) {
+                    error.nombre = 'Por favor introduce un nombre';
+                } else if(/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.nombre)){
+                    error.nombre = 'El nombre solo puede contener letras y espacios';
+                    }
+                return error;
+            }}
+            onSubmit={()=>{
+                console.log("enviado")
+            }}
+        >
+            {({values, errors, handleSubmit, handleChange,handleBlur}) => (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Nombre de usuario"
+                            value={values.nombre}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.nombre && <div>{errors.nombre}</div>}
+                    </div>
+                    <div>
+                        <label htmlFor="correo">Correo</label>
+                        <input
+                            type="text"
+                            id="correo"
+                            name="correo"
+                            placeholder="nombre@correo.com"
+                            value={values.correo}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {errors.correo && <div>{errors.correo}</div>}
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+            )}
+        </Formik>
     </div>
     )
 }
